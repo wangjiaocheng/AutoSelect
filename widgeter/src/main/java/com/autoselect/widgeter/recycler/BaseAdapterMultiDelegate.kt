bda0b72eb338f0f110f0e61d6c8680fc36979471
王教成 <wangjiaocheng@foxmail.com>
2020-09-27 15:46:39 +0800

    Initial commit

--- widgeter/src/main/java/com/autoselect/widgeter/recycler/BaseAdapterMultiDelegate.kt
+++ widgeter/src/main/java/com/autoselect/widgeter/recycler/BaseAdapterMultiDelegate.kt
@@ -5,12 +5,12 @@ import com.autoselect.widgeter.recycler.common.ViewHolderBase
 
 abstract class BaseAdapterMultiDelegate<T, VH : ViewHolderBase>(data: MutableList<T>? = null) :
     BaseAdapterQuick<T, VH>(0, data) {
-    var multiTypeDelegate: BaseTypeMultiDelegate<T>? = null
+    var typeMultiDelegate: BaseTypeMultiDelegate<T>? = null
     override fun getDefItemViewType(position: Int): Int =
-        checkNotNull(multiTypeDelegate?.getItemType(data, position))
+        checkNotNull(typeMultiDelegate?.getItemType(data, position))
         { "Please use setMultiTypeDelegate first!" }
 
     override fun onCreateDefViewHolder(parent: ViewGroup, viewType: Int): VH =
-        createBaseViewHolder(parent, checkNotNull(multiTypeDelegate?.getLayoutId(viewType))
+        createBaseViewHolder(parent, checkNotNull(typeMultiDelegate?.getLayoutId(viewType))
         { "Please use setMultiTypeDelegate first!" })
 }//多类型布局通过代理类方式，返回布局id和item类型，适用于:1、实体类不方便扩展，此Adapter数据类型可任意类型，只需BaseTypeDelegateMulti.getItemType返回对应类型；2、item类型较少，类型较多，为隔离各类型业务逻辑用BaseAdapterBinder。
