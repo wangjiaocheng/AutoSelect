44f3fbcc04d2b3b323856d1f360a1ebf3938c9be
王教成 <wangjiaocheng@foxmail.com>
2020-09-27 17:42:55 +0800

    Initial commit

--- widgeter/src/main/java/com/autoselect/widgeter/recycler/listener/BaseListener.kt
+++ widgeter/src/main/java/com/autoselect/widgeter/recycler/listener/BaseListener.kt
@@ -1,9 +1,9 @@
 package com.autoselect.widgeter.recycler.listener
 
 interface BaseListener {
+    fun setGridSpanSizeLookup(spanSizeLookup: GridSpanSizeLookup?)
     fun setOnItemClickListener(listener: OnItemClickListener?)
     fun setOnItemLongClickListener(listener: OnItemLongClickListener?)
     fun setOnItemChildClickListener(listener: OnItemChildClickListener?)
     fun setOnItemChildLongClickListener(listener: OnItemChildLongClickListener?)
-    fun setGridSpanSizeLookup(spanSizeLookup: GridSpanSizeLookup?)
 }//BaseQuickAdapter需要设置，用java定义兼容java写法
