1457bec61a0f7b5df80a939f32eab290f1aab90e
王教成 <wangjiaocheng@foxmail.com>
2020-10-26 11:50:29 +0800

    Initial commit

--- widgeter/src/main/java/com/autoselect/widgeter/flow/adapter/AdapterTab.kt
+++ widgeter/src/main/java/com/autoselect/widgeter/flow/adapter/AdapterTab.kt
@@ -1,6 +1,6 @@
 package com.autoselect.widgeter.flow.adapter
 
-abstract class AdapterTab<T>(layoutId: Int, data: MutableList<T?>?) :
+abstract class AdapterTab<T>(layoutId: Int, data: MutableList<Any>?) :
     AdapterTemplate<T?>(layoutId, data) {
     fun resetAllColor(viewId: Int, color: Int) =
         flowListenerAdapter?.resetAllTextColor(viewId, color)//恢复某个id颜色
